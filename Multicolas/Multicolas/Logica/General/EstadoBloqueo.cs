8ea9b75810cd8fea5f7efc063b49392924817f41
KevAlex <kevin.alexdiaz@hotmail.com>
2022-03-26 21:55:23 -0500

    Solo logica de RoundRobin y esqueleto agregados

---
+++ Multicolas/Multicolas/Logica/General/EstadoBloqueo.cs
@@ -0,0 +1,20 @@
+﻿namespace Multicolas.Logica.General
+{
+    public class EstadoBloqueo
+    {
+        //Evaluar si esta clase sobra
+        public Task BloquearProceso(Proceso procesoB)
+        {
+            // parece que sobra
+            procesoB.Bloqueado = true;
+            // para aumentarle el tiempo 
+            // parece que sobra
+            procesoB.FueBloqueado = true;
+            // Lo moví al index para renderizarlo mejor
+            //EstadoInicial.ListaEjecucion.Remove(procesoB);
+            EstadoInicial.ProcesosListos.Enqueue(procesoB);
+            Console.WriteLine("Proceso bloqueado " + procesoB.Name + " Rafaga Temporal " + procesoB.RafagaTemporal + " Espera " + procesoB.TiempoEspera);
+            return Task.CompletedTask;
+        }
+    }
+}
